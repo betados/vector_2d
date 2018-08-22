95f261ded7a185e89f9475c03a6cba5f6ee8c39a
Alvaro Torres <torrestal@gmail.com>
2018-08-22 14:38:34 +0200

    atributes made private

--- vector_2d/vector.py
+++ vector_2d/vector.py
@@ -5,47 +5,55 @@ from math import hypot, cos, sin
 
 class Vector(object):
     def __init__(self, x=0.0, y=0.0):
-        self.x = x
-        self.y = y
+        self.__x = x
+        self.__y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     def __repr__(self):
-        return type(self).__name__ + '(%r, %r)' % (self.x, self.y)
+        return type(self).__name__ + '(%r, %r)' % (self.__x, self.__y)
 
     def __abs__(self):
-        return hypot(self.x, self.y)
+        return hypot(self.__x, self.__y)
 
     def __add__(self, other):
-        x = self.x + other.x
-        y = self.y + other.y
+        x = self.__x + other.x
+        y = self.__y + other.y
         return Vector(x, y)
 
     def __sub__(self, other):
-        x = self.x - other.x
-        y = self.y - other.y
+        x = self.__x - other.x
+        y = self.__y - other.y
         return Vector(x, y)
 
     def __mul__(self, scalar):
-        return Vector(self.x * scalar, self.y * scalar)
+        return Vector(self.__x * scalar, self.__y * scalar)
 
     def get_unit(self):
         module = float(abs(self))
         if module == 0:
             module = 99999999
-        return Vector(self.x / float(module), self.y / float(module))
+        return Vector(self.__x / float(module), self.__y / float(module))
 
     def get_comps(self, f=True):
         if f:
-            return self.x, self.y
-        return int(self.x), int(self.y)
+            return self.__x, self.__y
+        return int(self.__x), int(self.__y)
 
     def set_comp(self, comp, value):
         if comp == 0:
-            self.x = value
+            self.__x = value
         else:
-            self.y = value
+            self.__y = value
 
     def __neg__(self):
-        return Vector(-self.x, -self.y)
+        return Vector(-self.__x, -self.__y)
 
     def __call__(self, comp=None):
         if comp is None:
@@ -53,6 +61,6 @@ class Vector(object):
         return self.get_comps()[comp]
 
     def __eq__(self, other):
-        if other.x == self.x and other.y == self.y:
+        if other.x == self.__x and other.y == self.__y:
             return True
         return False
