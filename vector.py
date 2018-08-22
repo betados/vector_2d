e1d6db376f10763fa09528d59d1ee0fa1f8a7dbc
Alvaro Torres <alvaro.torres@visualcounter.com.es>
2018-08-22 12:15:57 +0200

    first commit

---
+++ vector.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from math import hypot, cos, sin
+
+
+class Vector(object):
+    def __init__(self, x=0.0, y=0.0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector(%r, %r)' % (self.x, self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
+
+    def get_unit(self):
+        module = float(abs(self))
+        if module == 0:
+            module = 99999999
+        return Vector(self.x / float(module), self.y / float(module))
+
+    def get_comps(self, f=True):
+        if f:
+            return self.x, self.y
+        return int(self.x), int(self.y)
+
+    def set_comp(self, comp, value):
+        if comp == 0:
+            self.x = value
+        else:
+            self.y = value
+
+    def __neg__(self):
+        return Vector(-self.x, -self.y)
+
+    def __call__(self, comp=None):
+        if comp is None:
+            return self.get_comps()
+        return self.get_comps()[comp]
+
+    def _eq__(self, other):
+        if other.x == self.x and other.y == self.y:
+            return True
+        return False
+
+
+class VectorPolar(object):
+    def __init__(self, module, argument):
+        self.module = module
+        self.argument = argument
+
+    def to_cartesian(self):
+        return Vector(cos(self.argument), sin(self.argument)) * self.module
+
+    def __repr__(self):
+        return 'VectorPolar(%r, %r)' % (self.module, self.argument)
