7720c1de03263ccd11c08a2159b79793836e0199
Alvaro Torres <torrestal@gmail.com>
2018-08-22 14:16:58 +0200

    created setup.py to enable installation with pip

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='vector_2d',
+    version='0.0.1',
+    description='A module witch implements a two-dimensional vector, both in cartesian and polar coordinates.',
+    license='MIT',
+    packages=['vector_2D'],
+    author='Álvaro Torres Ochaita',
+    author_email='torrestal@gmail.com',
+    keywords=['vector'],
+    url='https://github.com/betados/vector_2D'
+)
