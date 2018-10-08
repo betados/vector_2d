696313bda855cc277def5ccff34a68690e6e1ceb
betados <torrestal@gmail.com>
2018-10-08 20:53:17 +0200

    import was imported the installed not the present file

--- vector_2d/__init__.py
+++ vector_2d/__init__.py
@@ -1,5 +1,5 @@
-from vector import *
-from vectorPolar import *
+from vector_2d.vector import *
+from vector_2d.vectorPolar import *
 
 __all__ = ["vector", "vectorPolar", "Vector", "VectorPolar", "round_vector", "angle", "distance_line_point",
            "distance_segment_point"]
