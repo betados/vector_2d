8933ec5987d8b394397265c05ac3935f8c7160f6
Alvaro Torres <torrestal@gmail.com>
2018-10-15 12:47:30 +0200

    setup changes

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import setup
 
 setup(
     name='vector_2d',
-    version='1.2.0',
+    version='1.2.1',
     description='A module witch implements a two-dimensional vector, both in cartesian and polar coordinates.',
     long_description=open('README.MD').read(),
     long_description_content_type='text/markdown',
     license='MIT',
-    packages=['vector_2D'],
+    packages=['vector_2d'],
     author='Álvaro Torres Ochaita',
     author_email='torrestal@gmail.com',
     keywords=['vector', '2d', 'physics', 'games', 'pygame'],
-    url='https://github.com/betados/vector_2D',
+    url='https://github.com/betados/vector_2d',
 )
 # python setup.py sdist bdist_wheel
 # twine upload dist/*
